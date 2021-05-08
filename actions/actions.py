2ecf26cf0f73e0a57da973acdf39ccb4da82787f
Kalmykova Anastasiia <kalmykova.anastasiia@gmail.com>
2021-05-08 23:07:50 +0200

    Updated API with world information data

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,21 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+import os.path
+import csv
+
+iso_file = os.path.join(os.path.dirname(__file__),"iso_countries.csv")
+locations_dict = dict()
+
+with open(iso_file, newline='') as csvfile:
+    isoreader = csv.reader(csvfile)
+    for row in isoreader:
+        locations_dict[row[0].lower()] = row[2]
+#print(locations_dict) 
+
+    
+#print("PATH",path)
+#print("FILE", __file__)
 #
 #
 # class ActionHelloWorld(Action):
@@ -49,28 +64,43 @@ class ActionInfectionNumbers(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
             
-        response = requests.get("https://api.covid19india.org/data.json").json()
-        #print(response)
+        #response = requests.get("https://api.covid19india.org/data.json").json()
+        #response = requests.get("https://covid-api.com/api/reports").json()
+
         entities = tracker.latest_message['entities']
-        
+        if not entities :
+            dispatcher.utter_message(text="Oops,sorry.The information is missing. Please try another country")
+            return []
+
         print("Last Message Now", entities)
         #output: [{'entity': 'country', 'start': 12, 'end': 18, 'confidence_entity': 0.9983867406845093, 'value': 'france', 'extractor': 'DIETClassifier'}]
+        
         country = None
         
         for e in entities:
             if e['entity'] == 'country':
-                country = e['value'] 
-                print(country)     
-        for data in response ["statewise"]:
-            #This API example has just Maharashtra state, we will use other API 
-            # and replace the variable state with "country", I used new variable state here
-            # just to be sure that API-response gives the corresponding output 
+                country = e['value'].lower()
+                print(country) 
+        #region = "Saarland"
+            
+        PARAMS = {'iso':locations_dict[country]} #,'region_province' :region}
+
+        #URL = "https://covid-api.com/api/reports" #gives the information about cities and regions
+        
+        URL = "https://covid-api.com/api/reports/total" #gives the information just in country
+        r = requests.get(url=URL,params=PARAMS)
+        r.raise_for_status()
+        
+        data = r.json()
+        print("DATA JSON: ", data)
+       
+        '''for data in response ["statewise"]: 
             state = "Maharashtra"
             if data["state"] == state.title():
                 print(data)
         
-        #dispatcher.utter_message(text="Take care of yourself and your family")
+        ''' 
+        dispatcher.utter_message(text="Take care of yourself and your family")
         print("This action is from Corona action")
-        dispatcher.utter_message(text="Take care of yourself and your family in " + state.title())
+        #dispatcher.utter_message(text="Take care of yourself and your family in " + state.title())
         return []
-
