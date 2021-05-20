3f6b1ca013fc1b215dd43b0cf387412189c3fad1
Kirchner <sven.kirchner98@gmail.com>
2021-05-20 22:25:41 +0200

    Serialized objects from apicreatedict.py, added greetings for some actions

--- actions/api_create_dict.py
+++ actions/api_create_dict.py
@@ -1,4 +1,5 @@
 import os.path
+import json
 import csv
 from collections import defaultdict
 
@@ -6,18 +7,25 @@ iso_file = os.path.join(os.path.dirname(__file__),"iso_countries.csv")
 iata_file = os.path.join(os.path.dirname(__file__),"IATA.csv")
 airport_dict = defaultdict(dict)
 locations_dict = dict()
+list_of_missing_country = []
 
-with open(iso_file, newline='') as csvfile_iso:
+
+def json_dump(myobject, newfilename):
+    with open(newfilename, mode='w', encoding='utf-16') as file:
+        json.dump(myobject, file)
+
+
+with open(iso_file, newline='', encoding = 'utf-8') as csvfile_iso:
     isoreader = csv.reader(csvfile_iso)
     for row in isoreader:
         locations_dict[row[0].lower()] = row[2]
 
-with open(iata_file,newline='') as csvfile_iata:
+with open(iata_file,newline='', encoding= 'utf-8') as csvfile_iata:
     iatareader = csv.reader(csvfile_iata)
     for row in iatareader:
         airport_dict[row[1].lower()][row[0].lower()] = row[2]
 
-list_of_missing_country = []
+
 count = 0
 for key_country in locations_dict.keys():
     if key_country not in airport_dict.keys():
@@ -27,3 +35,6 @@ for i in to_add:
     count += 1
 
 
+json_dump(airport_dict, 'airportdict.json')
+json_dump(locations_dict, 'locationsdict.json')
+json_dump(list_of_missing_country, 'missinglocations.json')
