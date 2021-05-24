5a2b57c25f70c6bdc6b58261eca839c03c35f0fa
Kirchner <sven.kirchner98@gmail.com>
2021-05-24 23:15:11 +0200

    Added API for city action, changed parts in nlu, Infection number still not recognized

--- actions/api_create_dict.py
+++ actions/api_create_dict.py
@@ -7,12 +7,12 @@ iata_file = os.path.join(os.path.dirname(__file__),"IATA.csv")
 airport_dict = defaultdict(dict)
 locations_dict = dict()
 
-with open(iso_file, newline='') as csvfile_iso:
+with open(iso_file, newline='', encoding = "utf-8") as csvfile_iso:
     isoreader = csv.reader(csvfile_iso)
     for row in isoreader:
         locations_dict[row[0].lower()] = row[2]
 
-with open(iata_file,newline='') as csvfile_iata:
+with open(iata_file,newline='', encoding = "utf-8") as csvfile_iata:
     iatareader = csv.reader(csvfile_iata)
     for row in iatareader:
         airport_dict[row[1].lower()][row[0].lower()] = row[2]
